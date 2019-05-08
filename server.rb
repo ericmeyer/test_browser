38d874d6cd4b598b110730d91f3d9fbdfa043ccb
Eric Meyer <emeyer@8thlight.com>
2019-05-07 19:34:02 -0500

    Display a static list of test names

--- server.rb
+++ server.rb
@@ -1,5 +1,13 @@
 require "sinatra"
 
+class Results
+
+  def self.get
+    JSON.parse(File.read("results.json"))
+  end
+
+end
+
 get "/" do
-  erb :index
+  erb :index, {locals: {results: Results.get}}
 end
